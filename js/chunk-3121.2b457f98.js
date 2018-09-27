2a9d1dca494aede0597540baf77eb3c6f227a1cc
saintmob <saintmob@gmail.com>
2018-09-27 19:14:23 +0800

    update new event html

---
+++ js/chunk-3121.2b457f98.js
@@ -0,0 +1,32 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([
+    ["chunk-3121"], { "038d": function(t, n, i) { t.exports = i.p + "img/img-2@3x.8fddeeae.png" }, "0b09": function(t, n, i) { "use strict"; var e = i("5bb6"),
+                s = i.n(e);
+            s.a }, "20d7": function(t, n, i) { t.exports = i.p + "img/img-5@3x.de057a4d.png" }, "30dc": function(t, n, i) { t.exports = i.p + "img/img-3@3x.ff7ab7e6.png" }, "5bb6": function(t, n, i) {}, "866b": function(t, n, i) { "use strict";
+            i.r(n); var e = function() { var t = this,
+                        n = t.$createElement,
+                        i = t._self._c || n; return i("div", { staticClass: "index" }, [i("div", { staticClass: "index-content" }, [i("div", { staticClass: "index-content-1" }, [i("img", { attrs: { src: t.img1 } })]), i("div", { staticClass: "index-content-2" }, [i("img", { attrs: { src: t.img2 } }), i("a", { attrs: { href: "https://www.sundayhi.com" } }, [t._v("前往下载")])]), i("div", { staticClass: "index-content-3" }, [i("img", { attrs: { src: t.img3 } })]), i("div", { staticClass: "index-content-4" }, [i("button", { on: { click: t.handleClick } }, [t._v("立即报名抽奖")])]), i("div", { staticClass: "index-content-5" }, [i("img", { attrs: { src: t.img4 } })]), i("div", { staticClass: "index-content-6" }, [t._m(0), i("div", { staticClass: "index-content-6R" }, [i("img", { attrs: { src: t.img5 } })])]), t._m(1), t._m(2)])]) },
+                s = [function() { var t = this,
+                        n = t.$createElement,
+                        i = t._self._c || n; return i("div", { staticClass: "index-content-6L" }, [i("p", { staticClass: "index-content-6L-1" }, [t._v("抽奖限广州在校大学生"), i("br"), t._v("\n        报名截止：2018年10月5日 23:55"), i("br"), t._v("\n        欢迎给 POP IM 提意见(hi@sunday37.com)，我们会挑选 50 位用心反馈的同学，赠予京东购物卡￥200。"), i("br"), i("br"), t._v("\n        微信客服：popim37")])]) }, function() { var t = this,
+                        n = t.$createElement,
+                        i = t._self._c || n; return i("div", { staticClass: "index-content-7" }, [t._v("\n      1. 参加抽奖活动者在提交个人信息前，请确保已经阅读并且愿意遵守相关活动规则。"), i("br"), t._v("\n      2. 抽奖活动的获奖通知将在结果公示后 3 个工作日内，发送至报名时预留的邮箱地址，请确认提供真实有效的联系方式，领奖人必须携带真实有效的学生证参与领奖。"), i("br"), t._v("\n      3. 获奖者必须配合主办方完成相应的奖品发放工作。"), i("br"), t._v("\n      4. 若获奖者在限定时间内对获奖通知未做回应，则视为放弃奖项，由其他获奖者取代。"), i("br"), t._v("\n      5. 活动主办方对本活动保留最终解释权。\n    ")]) }, function() { var t = this,
+                        n = t.$createElement,
+                        i = t._self._c || n; return i("div", { staticClass: "index-content-8" }, [i("a", { attrs: { href: "http://gdcainfo.miitbeian.gov.cn", target: "_blank" } }, [t._v("粤ICP备18070086号-1")]), i("p", [t._v("© 2018 Sunday Morning Co.,Ltd.")])]) }],
+                c = i("d634"),
+                a = i.n(c),
+                r = i("038d"),
+                d = i.n(r),
+                o = i("30dc"),
+                m = i.n(o),
+                v = i("f1e6"),
+                g = i.n(v),
+                l = i("20d7"),
+                f = i.n(l),
+                u = { name: "index", data: function() { return { img1: a.a, img2: d.a, img3: m.a, img4: g.a, img5: f.a } }, methods: { handleClick: function() { window.location.href = "http://cn.mikecrm.com/8zdnYwx" } } },
+                p = u,
+                _ = (i("0b09"), i("2877")),
+                x = Object(_["a"])(p, e, s, !1, null, "fe7c4072", null);
+            x.options.__file = "index.vue";
+            n["default"] = x.exports }, d634: function(t, n, i) { t.exports = i.p + "img/img-1@3x.f3ca2a6d.png" }, f1e6: function(t, n, i) { t.exports = i.p + "img/img-4@3x.70a1bc02.png" } }
+]);
+//# sourceMappingURL=chunk-3121.2b457f98.js.map
